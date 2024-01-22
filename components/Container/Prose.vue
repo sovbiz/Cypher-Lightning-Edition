a1984b4767b798ebaaf480f3c9e88a4641eaea1e
Melissa <M371554@pm.me>
2024-01-22 21:16:32 +0100

    Update Prose.vue

--- components/Container/Prose.vue
+++ components/Container/Prose.vue
@@ -2,7 +2,7 @@
 </script>
 <template>
   <div
-    class="max-w-4xl dark:prose-invert prose lg:prose-xl dark:lg:proxe-xl-invert mx-auto dark:text-white pb-16 px-5 md:px-12 mt-24"
+    class="max-w-4xl dark:prose-invert prose md:prose-lg lg:prose-xl dark:lg:proxe-xl-invert mx-auto dark:text-white pb-16 px-5 md:px-12 mt-24"
   >
     <ContentSlot :use="$slots.default" unwrap="p" />
   </div>
