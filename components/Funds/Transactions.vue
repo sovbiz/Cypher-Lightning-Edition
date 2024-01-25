27d7300d6611ad3d7a818b1f12713cba4f1e8a4f
Melissa <M371554@pm.me>
2024-01-25 15:38:15 +0100

    Update Transactions.vue

--- components/Funds/Transactions.vue
+++ components/Funds/Transactions.vue
@@ -131,6 +131,7 @@ const invoices = await $fetch("https://api.getalby.com/invoices", {
   console.log(error);
 });
 
+console.log(invoices)
 // const date = new Date(timestamp);
 // const options = { year: 'numeric', month: 'long', day: 'numeric' };
 // const formattedDate = new Intl.DateTimeFormat('en-US', options).format(date);
