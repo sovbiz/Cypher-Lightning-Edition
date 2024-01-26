727b1c661a4c6798338bbea2a86c50b2f14e8d5f
Melissa <M371554@pm.me>
2024-01-26 20:22:40 +0100

    Finished Lightning Widget

--- components/Funds/Invoicewidget.vue
+++ components/Funds/Invoicewidget.vue
@@ -1,34 +1,34 @@
 <template>
-    <div class="isolate bg-white px-6 py-24 sm:py-32 lg:px-8">
+    <div class="isolate bg-colorBgLight dark:bg-colorBgDark px-6 py-24 sm:py-32 lg:px-8">
       <div class="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]" aria-hidden="true">
         <div class="relative left-1/2 -z-10 aspect-[1155/678] w-[36.125rem] max-w-none -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-40rem)] sm:w-[72.1875rem]" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)" />
       </div>
       <div class="mx-auto max-w-2xl text-center">
-        <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Lightning Invoice Creator</h2>
-        <p class="mt-2 text-lg leading-8 text-gray-600">Stuff a invoice with memo's & payer details</p>
+        <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl dark:text-white ">Lightning Invoice Creator</h2>
+        <p class="mt-2 text-lg leading-8 text-gray-600 dark:text-white ">Stuff a invoice with a comment</p>
       </div>
-      <form action="#" method="POST" class="mx-auto mt-16 max-w-xl sm:mt-20">
+      <div class="mx-auto mt-16 max-w-xl sm:mt-20">
         <div class="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
           <div>
-            <label for="lnurl" class="block text-sm font-semibold leading-6 text-gray-900">Lightning Address</label>
+            <label for="lnurl" class="block text-sm font-semibold leading-6 text-gray-900 dark:text-white ">Lightning Address</label>
             <div class="mt-2.5">
-              <input type="text" name="lnurl" id="lnurl" autocomplete="lnurl" class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+              <input type="text" v-model="to" name="lnurl" id="lnurl" autocomplete="lnurl" class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
             </div>
           </div>
           <div>
-            <label for="last-name" class="block text-sm font-semibold leading-6 text-gray-900">Ammount (in millisats)</label>
+            <label for="last-name" class="block text-sm font-semibold leading-6 text-gray-900 dark:text-white ">Ammount (in millisats)</label>
             <div class="mt-2.5">
-              <input type="text" name="last-name" id="last-name" autocomplete="family-name" class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+              <input type="text" v-model="amount" name="last-name" id="last-name" autocomplete="family-name" class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
             </div>
           </div>
 
           <div class="sm:col-span-2">
-            <label for="message" class="block text-sm font-semibold leading-6 text-gray-900">Message</label>
+            <label for="message" class="block text-sm font-semibold leading-6 text-gray-900 dark:text-white ">Message</label>
             <div class="mt-2.5">
-              <textarea name="message" id="message" rows="4" class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+              <textarea name="message" v-model="comment" id="message" rows="4" class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
             </div>
           </div>
-          <SwitchGroup as="div" class="flex gap-x-4 sm:col-span-2">
+          <!-- <SwitchGroup as="div" class="flex gap-x-4 sm:col-span-2">
             <div class="flex h-6 items-center">
               <Switch v-model="agreed" :class="[agreed ? 'bg-indigo-600' : 'bg-gray-200', 'flex w-8 flex-none cursor-pointer rounded-full p-px ring-1 ring-inset ring-gray-900/5 transition-colors duration-200 ease-in-out focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600']">
                 <span class="sr-only">Agree to policies</span>
@@ -40,12 +40,15 @@
               {{ ' ' }}
               <a href="#" class="font-semibold text-indigo-600">privacy&nbsp;policy</a>.
             </SwitchLabel>
-          </SwitchGroup>
+          </SwitchGroup> -->
         </div>
         <div class="mt-10">
-          <button type="submit" class="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Let's Go ! ⚡️</button>
+          <button type="submit" @click="handleSubmit" class="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Let's Go ! ⚡️</button>
         </div>
-      </form>
+
+        <img v-if="qrCodeDataUrl" :src="qrCodeDataUrl" alt="QR Code" class="w-48 mx-auto mt-12" />
+
+      </div>
     </div>
   </template>
   
@@ -54,5 +57,47 @@
   import { ChevronDownIcon } from '@heroicons/vue/20/solid'
   import { Switch, SwitchGroup, SwitchLabel } from '@headlessui/vue'
   
-  const agreed = ref(false)
-  </script>
+  import QRCode from "qrcode";
+
+// Define the data properties
+const to = ref('')
+const amount = ref('')
+const comment = ref('')
+const invoice = ref('')
+const qrCodeDataUrl = ref('');
+
+// Method to handle form submission
+const handleSubmit = async () => {
+
+
+    const response = await fetch('https://proxy.lightning.eu/generate-invoice?' + `ln=${to.value}&amount=${amount.value}&comment=${comment.value}`, {
+      method: 'GET',
+      headers: {
+        "Accept": "application/json",
+      "Content-Type": "application/json",
+      },
+    })
+
+    
+    invoice.value = await response.json()
+    // invoice.value = invoiceurl._value.invoice.pr
+    // console.log(invoice._value.invoice.pr)
+
+    QRCode.toDataURL(invoice._value.invoice.pr, (error, dataUrl) => {
+  if (error) {
+    console.error("Error generating QR code:", error);
+  } else {
+    qrCodeDataUrl.value = dataUrl;
+  }
+});
+
+}
+
+
+
+
+
+
+
+
+</script>
