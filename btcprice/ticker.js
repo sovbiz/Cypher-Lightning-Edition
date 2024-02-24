8a2b606de8986b64396f2a730ed923694e762135
Melissa <M371554@pm.me>
2024-02-24 14:29:25 +0100

    Ticker fix decimal / open finance setting

--- btcprice/ticker.js
+++ btcprice/ticker.js
@@ -10,7 +10,7 @@ export function ticker() {
       try {
         const response = await $fetch("https://app.yieldmonitor.io/api/v2/symbol/ym/33913");
         btcprice.value = (Number(response.symbols[0].price)).toFixed(2); // update the state with the fetched data
-        onedollarsatprice.value = (1 / (Number(response.symbols[0].price))).toFixed(8) * 100000000; // update the state with the fetched data
+        onedollarsatprice.value = ((1 / (Number(response.symbols[0].price))).toFixed(8) * 100000000).toFixed(0); // update the state with the fetched data
         onedollarbtcprice.value = (1 / (Number(response.symbols[0].price))).toFixed(8) ; // update the state with the fetched data
 
       } catch (error) {
